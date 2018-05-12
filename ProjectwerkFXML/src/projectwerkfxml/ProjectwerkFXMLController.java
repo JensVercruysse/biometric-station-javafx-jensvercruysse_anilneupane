8314f7e65b7fc0d6a53df03c5e3f20fb2206861f
Jens Vercruysse <jensvercruysse@outlook.com>
2018-05-12 19:46:49 +0200

    test

--- ProjectwerkFXML/src/projectwerkfxml/ProjectwerkFXMLController.java
+++ ProjectwerkFXML/src/projectwerkfxml/ProjectwerkFXMLController.java
@@ -40,7 +40,6 @@ public class ProjectwerkFXMLController implements Initializable, IMqttDataHandle
     }
 
     @Override
-<<<<<<< HEAD
     public void initialize(URL url, ResourceBundle rb) {
         jens_heartbeat = new MqttBiometricDataService("jens", "heartbeat");
         jens_heartbeat.setDataHandler(this);
@@ -52,24 +51,10 @@ public class ProjectwerkFXMLController implements Initializable, IMqttDataHandle
         jens_y_value.setDataHandler(this);
         jens_z_value = new MqttBiometricDataService("jens", "acc_z_value");
         jens_z_value.setDataHandler(this);
-=======
-    public void initialize(URL url, ResourceBundle rb) {        
-        jens_heartbeat = new MqttChatService("jens", "jens_heartbeat");
-        jens_heartbeat.setMessageHandler(this);
-        jens_temperature = new MqttChatService("jens", "jens_temperature");
-        jens_temperature.setMessageHandler(this);
-        jens_x_value = new MqttChatService("jens", "jens_x_value");
-        jens_x_value.setMessageHandler(this);
-        jens_y_value = new MqttChatService("jens", "jens_y_value");
-        jens_y_value.setMessageHandler(this);
-        jens_z_value = new MqttChatService("jens", "jens_z_value");
-        jens_z_value.setMessageHandler(this);
->>>>>>> 3167ffee5d046b79b5cba76e827aec12fdc15e4a
         disconnectClientOnClose();
     }
 
     @Override
-<<<<<<< HEAD
     public void dataArrived(String channel, String data) {
         System.out.println("Received data (on channel = " + channel + "): " + data);
 
@@ -83,22 +68,7 @@ public class ProjectwerkFXMLController implements Initializable, IMqttDataHandle
             }
         });
     }
-
-=======
-    public void messageArrived(String channel, String message) {
-        System.out.println("Received chat messages (on channel = " + channel + "): " + message);  
-        
-        // Om van die cross-thread error af te komen heb je onderstaand stukje code nodig.
-        // Gevonden op stack-overflow https://stackoverflow.com/questions/11690683/javafx-update-ui-from-another-thread
-        Platform.runLater(new Runnable() {
-            @Override public void run() {
-              //Update UI here     
-              heartbeat.setText(message);
-            }
-        });
-    }    
     
->>>>>>> 3167ffee5d046b79b5cba76e827aec12fdc15e4a
     private void disconnectClientOnClose() {
         // Source: https://stackoverflow.com/a/30910015
         start.sceneProperty().addListener((observableScene, oldScene, newScene) -> {
@@ -109,15 +79,10 @@ public class ProjectwerkFXMLController implements Initializable, IMqttDataHandle
                         // stage is set. now is the right time to do whatever we need to the stage in the controller.
                         ((Stage) newWindow).setOnCloseRequest((event) -> {
                             jens_heartbeat.disconnect();
-<<<<<<< HEAD
                             jens_temperature.disconnect();
                             jens_x_value.disconnect();
                             jens_y_value.disconnect();
                             jens_z_value.disconnect();
-=======
-                            
-                            // Hier moet je ook je andere services disconnecten !!!!
->>>>>>> 3167ffee5d046b79b5cba76e827aec12fdc15e4a
                         });
                     }
                 });
